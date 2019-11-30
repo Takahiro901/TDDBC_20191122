f46c1a654e56deb388fcd4a9d93e9acd27f51d6f
Takahiro901 <30286581+Takahiro901@users.noreply.github.com>
2019-11-30 05:49:04 -0800

    first commit

---
+++ TDDBC_Scheduler/Scheduler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace TDDBC_Scheduler
+{
+    public class SchedulerSetting
+    {
+        private string timeString;
+        public SchedulerSetting()
+        {
+        }
+
+        public SchedulerSetting(string hour, string minutes, string second)
+        {
+            timeString = buildTimeString(hour, minutes, second);
+        }
+
+        public string GetTimeString()
+        {
+            return timeString;
+        }
+
+        public bool TaskIsExcutable(string hour, string minutes, string second)
+        {
+            string givenTime = buildTimeString(hour, minutes, second);
+
+            return timeString == givenTime;
+        }
+
+        private string buildTimeString(string hour, string minutes, string second)
+        {
+            return second + " " + minutes + " " + hour;
+
+        }
+    }
+}
---
+++ TDDBC_Scheduler_Test/UnitTest1.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDBC_Scheduler;
+
+namespace TDDBC_Scheduler_Test
+{
+    [TestClass]
+    public class SchedulerTest
+    {
+        private SchedulerSetting defaultSchedulerSetting;
+        [TestInitialize]
+        public void Initialize()
+        {
+            defaultSchedulerSetting = new SchedulerSetting("18", "9", "32");
+        }
+        
+
+        [TestMethod]
+        public void 文字列表現を取得する()
+        {
+            //実行
+            string actual = defaultSchedulerSetting.GetTimeString();
+
+            //検証
+            Assert.AreEqual("32 9 18", actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("32", "9", "18")]
+        [DataRow("1", "12", "7")]
+        [DataRow("0", "0", "0")]
+        public void 時刻を与えて設定された時刻と一致する(string a, string b, string c)
+        {
+            //準備
+            var schedulerSetting = new SchedulerSetting(a, b, c);
+
+            //実行
+            bool isSame = schedulerSetting.TaskIsExcutable(a, b, c);
+
+            //検証
+            Assert.AreEqual(true, isSame);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("32", "9", "18")]
+        [DataRow("1", "12", "7")]
+        [DataRow("0", "0", "0")]
+        public void 時刻を与えて設定された時刻と一致しない(string a, string b, string c)
+        {
+            //実行
+            bool isSame = defaultSchedulerSetting.TaskIsExcutable(a, b, c);
+
+            //検証
+            Assert.AreEqual(false, isSame);
+        }
+
+    }
+}
